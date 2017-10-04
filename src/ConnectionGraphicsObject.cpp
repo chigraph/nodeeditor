d10f2af6497ea5ac4bc03a0e7a81a8e786c647be
Russell Greene <russellgreene8@gmail.com>
2017-10-03 20:20:47 -0600

    Fix memory use-after-free error

--- src/ConnectionGraphicsObject.cpp
+++ src/ConnectionGraphicsObject.cpp
@@ -247,8 +247,9 @@ mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
     if (state().requiresPort()) {
       Q_ASSERT(this == _scene._temporaryConn);
       // remove this from the scene
+      auto& scene = _scene; // cache this to avoid use-after-free error
       delete _scene._temporaryConn;
-      _scene._temporaryConn = nullptr;
+      scene._temporaryConn = nullptr;
     }
     
     return;
@@ -265,15 +266,17 @@ mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
     node->resetReactionToConnection();
     Q_ASSERT(this == _scene._temporaryConn);
 
+    auto& scene = _scene; // cache this to avoid use-after-free error
     delete _scene._temporaryConn;
-    _scene._temporaryConn = nullptr;
+    scene._temporaryConn = nullptr;
   }
   else if (state().requiresPort())
   {
     Q_ASSERT(this == _scene._temporaryConn);
 
+    auto& scene = _scene; // cache this to avoid use-after-free error
     delete _scene._temporaryConn;
-    _scene._temporaryConn = nullptr;
+    scene._temporaryConn = nullptr;
   }
 }
 
